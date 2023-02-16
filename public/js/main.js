4852d6c93fda981d486a95b12f84eab03cbee202
dendonpaul <dendonpaul@gmail.com>
2023-02-16 21:33:17 +0530

    chat display and auto scroll to bottom added

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,11 @@
 const chatForm = document.getElementById("chat-form");
+const chatMessages = document.querySelector(".chat-messages");
+
 const socket = io();
 
 socket.on("message", (message) => {
   outputMessage(message);
+  chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
 chatForm.addEventListener("submit", (e) => {
@@ -14,4 +17,12 @@ chatForm.addEventListener("submit", (e) => {
 //Output Message Function
 const outputMessage = (message) => {
   const div = document.createElement("div");
+  div.classList.add("message");
+  div.innerHTML = `
+    <p class="meta">Brad <span>9:12pm</span></p>
+    <p class="text">
+    ${message}
+    </p>
+    `;
+  chatMessages.appendChild(div);
 };
